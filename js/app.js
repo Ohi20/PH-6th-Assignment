6c48a3a74e4efea9c66703235c60a21e94b520e3
Ohi97 <ashrafohi20@gmail.com>
2022-03-01 17:22:50 +0600

    Details button added

--- js/app.js
+++ js/app.js
@@ -14,39 +14,44 @@ const showPhoneDetails = (phones) => {
     div.innerHTML = `
     <div class="container mt-5">
         <div class="card-group container-fluid">
-            <div id="phone-container1" class="card phone-container border-3">
+            <div class="card phone-container border-3">
               <div class="card-body">
-                <h2 class="card-title">Name:${phone.phone_name} </h2>
-                <h4 class="card-text">Brand:${phone.brand} </h4>
-                <h4 class="card-text">Slug:${phone.slug} </h4>
+                <h2 class="card-title">Name: ${phone.phone_name} </h2>
+                <h4 class="card-text">Brand: ${phone.brand} </h4>
+                <h4 class="card-text">Slug: ${phone.slug} </h4>
                
               </div>
               <div>
-              <button>Details</button>
+              <button onclick="phoneDetails()" id="card-button" >Details</button>
               </div>
             </div>
-            <div id="phone-container2" class="card phone-container border-3">
+            <div class="card phone-container border-3">
             
               <div class="card-body">
                 <img src="${phone.image}" class="card-img-top w-75" alt="...">
                 
               </div>
-              <button>Details</button>
+             
             </div>
             <div class="card border-3">
               
               <div class="card-body">
                 <h3 class="card-title">Brand: </h3>
-               
-              </div>
-              <button>Details</button>
-            </div>
+            
           </div>
        </div>
     `
     parent.appendChild(div);
     }
     console.log(phones);
+}
+
+const phoneDetails = () => {
+    console.log("Cliked");
+    const url = `https://openapi.programming-hero.com/api/phone/${'card-button'}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(value => console.log(value))
 }
 
 
