9f61fbebaac1966e81e30a4585abeef2be1c3378
Ohi97 <ashrafohi20@gmail.com>
2022-03-02 12:30:36 +0600

    for each

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 const allPhone = () => {
-    const searchValue = document.getElementById("search-box").value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
-   
+    const searchValue = document.getElementById("search-box");
+    const searchText = searchValue.value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    searchValue.value = '';
     fetch(url)
     .then(res => res.json())
     .then(data => showPhoneDetails(data.data))
+    // console.log(url);
+    //  const keys = Object.keys(data.data);
+    // console.log(res.json());
+    // showPhoneDetails(data.data)
+    //  console.log(keys);
+       
 }
 
 const showPhoneDetails = (phones) => {
-   for (const phone of phones) {
+   console.log(phones);
+   phones.forEach(phone => {
     const parent = document.getElementById("phone-container");
     const div = document.createElement('div');
     div.innerHTML = `
@@ -17,12 +25,12 @@ const showPhoneDetails = (phones) => {
             <div class="card phone-container border-3">
               <div class="card-body">
                 <h2 class="card-title">Name: ${phone.phone_name} </h2>
-                <h4 class="card-text">Brand: ${phone.brand} </h4>
-                <h4 class="card-text">Slug: ${phone.slug} </h4>
+                
+                
                
               </div>
               <div>
-              <button onclick="phoneDetails()" id="card-button" >Details</button>
+              <button onclick="phoneDetails('hello')" id="card-button" >Details</button>
               </div>
             </div>
             <div class="card phone-container border-3">
@@ -36,22 +44,25 @@ const showPhoneDetails = (phones) => {
             <div class="card border-3">
               
               <div class="card-body">
-                <h3 class="card-title">Brand: </h3>
+                <h3 class="card-title">Brand: ${phone.brand} </h3>
             
           </div>
        </div>
     `
     parent.appendChild(div);
-    }
-    console.log(phones);
+   }); 
+   
+    //  console.log(phone);
+    //  console.log(phones);
+    // console.log(phone.slug);
 }
 
-const phoneDetails = () => {
-    console.log("Cliked");
-    const url = `https://openapi.programming-hero.com/api/phone/${'card-button'}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(value => console.log(value))
+const phoneDetails = (phoneId) => {
+    console.log(phoneId);
+    // const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    // fetch(url)
+    // .then(res => res.json())
+    // .then(value => console.log(value))
 }
 
 
